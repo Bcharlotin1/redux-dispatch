1e7eba45d1fc6635a3f3a6ac05ed20f8694ff697
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-24 16:48:51 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,23 @@
-function changeState(state, action){
+let state = { count: 0 };
+
+function changeState(state, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action)
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+render();
+
+// First, it persisted changes to our state. This is because we called the dispatch function, the dispatch function called our reducer, and then we took the return value from the reducer and assigned it to be our new state.
